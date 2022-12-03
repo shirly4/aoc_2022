7aaea9344b6e0afddf1e115267352013ea400d43
Shirly Israel <shirlyisrael@macbook-air-de-shirly.home>
2022-12-03 13:44:20 +0100

    Use context manager to open file & fix mapping of priority letters

--- day3/day3.py
+++ day3/day3.py
@@ -1,35 +1,30 @@
 import string
 
 def main():
-    f=open('advent_oc/day3/input.txt','r').read().splitlines()
-    
+    with open("input.txt", "r")  as f:
+        lines = f.read().splitlines()
+
+    all_letters=string.ascii_letters
+    alphabet={letter:index for index, letter in enumerate(all_letters,1)}
+
     #answer 1
     answer1=0
-    for rucksack in f:
+    for rucksack in lines:
         item1=list(rucksack[0:len(rucksack)//2])
         item2=list(rucksack[len(rucksack)//2:])
-        common_letter=''.join(set(list(item1)).intersection(list(item2)))
-        alphabet=dict(enumerate(string.ascii_letters,1))
-        priority=[priority for priority in alphabet if alphabet[priority]==common_letter]
-        answer1+=priority[0]
+
+        common_letter=''.join(set(item1).intersection(item2))
+        answer1+=alphabet[common_letter]
 
     print(answer1)
 
     #answer 2
     answer2=0
-    for elf in range (0,len(f),3):
-        common_letter=''.join(set(f[elf]).intersection(list(f[elf+1])).intersection(list(f[elf+2])))
-        priority=[priority for priority in alphabet if alphabet[priority]==common_letter]
-        answer2+=priority[0]
+    for elf in range (0,len(lines),3):
+        common_letter=''.join(set(lines[elf]).intersection(list(lines[elf+1]),list(lines[elf+2])))
+        answer2+=alphabet[common_letter]
 
     print(answer2)    
-
-        
-
-
-
-
-
 
 if __name__=="__main__":
     main()
